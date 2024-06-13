386551f7585693b028a43ee2fe0e98ac6ce502ff
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-06-13 21:00:07 +0200

    Rework Windows tag/version code to be more robust

--- go-build.cmd
+++ go-build.cmd
@@ -21,36 +21,41 @@ for %%x in (%package_split%) do set package_name=%%x
 
 rem echo PACKAGE_NAME=%package_name%    
 
-rem Now work out some tag information which we will parse into a variable and use later.
-rem This should tally with the overall version info, but with just the tagname, not the full information
+rem Now work out the most recent tag which we will parse into a variable and use later.
+rem We will use this in the executable name, and also to get the more detailed build version number
 for /F "tokens=*" %%g in ('git tag "--sort=taggerdate"') do (
-  SET tag_string=%%g
-  rem echo Version: -%tag_string%-
+  set tag_string=%%g
 )
 
 if "%tag_string%"=="" (
     echo No tag available.
-    set tag_string=
-) else (
-    rem Add a prefix to format the application name nicely
-    set tag_string=-%tag_string%
+    set tag_string=unknown
+    set version_string=unknown
+    goto :notag
 )
+
+echo Building for tag %tag_string%
 
 rem Now work out some version information which we will parse into a variable and use later.
 rem Wrap the arguments to git describe in quotes as the --abbrev clause seems to require it
-rem Requires the folloing in the 'main' package:
+rem Requires the following in the 'main' package:
 rem var version string
-rem The tag portion of this info should tally with the tag acquired above, but also contain commit info
-for /F "tokens=*" %%g in ('git describe "--tags" "--long" "--abbrev=7"') do (
-  SET version_string=%%g
-  rem echo Version: -%version_string%-
+rem ** could also consider:
+rem    git for-each-ref refs/tags --sort=-taggerdate --format='%(refname:lstrip=2)' --count=1
+rem    git for-each-ref refs/tags --sort=-taggerdate --format='%(refname:short)' --count=1
+for /F "tokens=*" %%g in ('git describe --all --long "--abbrev=7" %tag_string%') do (
+  set version_string=%%g
 )
+
+rem Safely rip the "tags/" from the git describe value we just obtained
+set "version_string=%version_string:tags/=%"
 
 if "%version_string%"=="" (
     echo No version tag available.
     set version_string=unknown
 )
 
+:notag
 rem List of platforms we would like to build for
 rem Supported values can be obtained by running "go tool dist list"
 rem Value for current system is shown with "go version" 
@@ -77,7 +82,7 @@ for %%y in (%platforms%) do (
     rem echo GOARCH=!GOARCH!
 
     rem Name the built application using platform details, and add .exe for Windows 
-    set output_name=%package_name%%tag_string%-!GOOS!-!GOARCH!
+    set output_name=%package_name%-%tag_string%-!GOOS!-!GOARCH!
     if "!GOOS!"=="windows" set output_name=!output_name!.exe
 
     rem Invoke the build command and check the outcome
